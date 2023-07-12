008e7917cc223f3a100a7474565b5c2288666dab
phil <phil@gmail.com>
2023-07-12 16:19:48 +0800

    first commit

---
+++ src/main/java/com/woniu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.woniu.git;/**
+ * @Description: GitTest
+ * @Author Phil
+ * @Date 2023/7/12 16:16
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
